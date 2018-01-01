1f635b44cf3123e92edb734d1c542440518fcf7b
Yehia Saleh <yehia.saleh@yale.edu>
2017-12-31 21:16:14 -0500

    Deployment test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send({name: 'Yehia'});
+});
+
 // Dynamic port specification for Heroku depolyment
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
